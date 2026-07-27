6a7bcee8b026ffd18900297753562ac4899b4a2d
pauel <pau.lacazedieu@gmail.com>
2026-07-27 08:10:21 +0200

    Builds on latest game version (NOT ALL BUGS SQUASHED)

--- GW-server-plugin/GW-server-plugin.cs
+++ GW-server-plugin/GW-server-plugin.cs
@@ -224,8 +224,8 @@ public class GwServerPlugin : BaseUnityPlugin
             return;
         }
         
-        Logger.LogDebug($"{player.PlayerName} : {player.SteamID} - joined the game");
-        var originalName = player.PlayerName;
+        Logger.LogDebug($"{player.GetPlayerName().SanitizedName} : {player.SteamID} - joined the game");
+        var originalName = player.GetPlayerName().SanitizedName;
         PlayerUtils.ApplyOrRemoveStaffTag(player);
         // Apply identification tag if not a staff member
         if (!PlayerUtils.IsStaff(player))
@@ -233,7 +233,7 @@ public class GwServerPlugin : BaseUnityPlugin
             PlayerIdentifier.AssignNewPlayer(player);
             PlayerUtils.ApplyIdentificationTag(player, PlayerIdentifier.GetPlayerId(player));
         }
-        Logger.LogInfo($"{player.PlayerName} : {player.SteamID} - joined the game");
+        Logger.LogInfo($"{player.GetPlayerName().SanitizedName} : {player.SteamID} - joined the game");
         var log = new JoinLeaveLog
         {
             SteamID = player.SteamID,
@@ -246,14 +246,14 @@ public class GwServerPlugin : BaseUnityPlugin
 
     private static void OnPlayerLeave(Player player)
     {
-        Logger.LogInfo($"{player.PlayerName} : {player.SteamID} - left the game");
+        Logger.LogInfo($"{player.GetPlayerName().SanitizedName} : {player.SteamID} - left the game");
         MissionVote.RemoveVoter(player.SteamID);
         PlayerIdentifier.RemovePlayer(player);
         var log = new JoinLeaveLog
         {
             SteamID = player.SteamID,
             IsOn = false,
-            Name = player.PlayerName,
+            Name = player.GetPlayerName().SanitizedName,
             Time = DateTime.UtcNow.ToTimestamp(),
             Score = (float)Math.Round(player.PlayerScore, 2)
         };
@@ -273,7 +273,7 @@ public class GwServerPlugin : BaseUnityPlugin
     
     internal static void OnPlayerTeamkill(Player killer, Player killed, string weaponName)
     {
-        OnTeamkill(killer, killed.PlayerName, weaponName);
+        OnTeamkill(killer, killed.GetPlayerName().SanitizedName, weaponName);
     }
 
     /// <summary>
